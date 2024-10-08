a409c2a9d7aea7d4a703fd52168e58d988fde74b
Yvan-David <irankundayvan2020@gmail.com>
2024-10-08 19:55:54 +0200

    ft-implement-subscription

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,30 @@
-import { Button } from "@nextui-org/button";
+'use client'
 import NextLink from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import twitter from "../public/TwitterX.png";
 import linkedin from "@/public/LinkedIn.png";
 import instagram from "@/public/Instagram.png";
 import Image from "next/image";
-import { Input } from "@nextui-org/input";
 import { Instagram, Link, Linkedin, Twitter, X } from "lucide-react";
+import { Form, Input, Button, message } from 'antd';
+import axios from 'axios';
+import { BASE_API_URL } from '../utils/constants';
 
 function Footer() {
+	const [loading, setLoading] = useState(false);
+	const [form] = Form.useForm();
+	const handleNewsletterSignUp = async(values: { names: string; email: string }) =>{
+		setLoading(true);
+		try {
+			const response = await axios.post(`${BASE_API_URL}/api/subscribe`, values);
+			message.success('Thanks for subscribing to our newsletter.');
+			form.resetFields(); 
+		  } catch (error) {
+			message.error('Failed to sign up. Please try again.');
+		  } finally {
+			setLoading(false);
+		  }
+	}
 	return (
 		<div className="bg-[#303950] w-full flex flex-col mt-10">
 			<div className="flex justify-around mx-[5%] mt-[5%] mb-[2%] text-white max-sm:flex-wrap max-sm:gap-5">
@@ -50,19 +66,40 @@ function Footer() {
 						Subscribe to our newsletter to get our news & deals delivered to
 						you.
 					</p>
-					<div className="flex mt-4 w-[100%]">
-						<Input
-							type="email"
-							placeholder="Email address"
-							radius="none"
-							className="text-[#9E9E9E] w-full"
-						/>
-						<Button
-							radius="none"
-							className="bg-primary px-4 -mr-[0.8rem] rounded-none text-white max-sm:px-10"
-						>
-							Submit
-						</Button>
+					<div>
+						<Form
+							form={form}
+							layout="vertical"
+							onFinish={handleNewsletterSignUp}
+							>
+							<Form.Item
+								name="names"
+								className="flex mt-4 w-[100%]"
+								rules={[{ required: true, message: 'Please input your name!' }]}
+							>
+								<Input placeholder="Name" className="text-[#000000] w-[100%]"/>
+							</Form.Item>
+							<Form.Item
+								name="email"
+								className="flex mt-4 w-[100%]"
+								rules={[
+								{ required: true, message: 'Please input your email!' },
+								{ type: 'email', message: 'Please enter a valid email!' },
+								]}
+							>
+								<Input placeholder="Email address" className="text-[#000000] w-[100%]"/>
+							</Form.Item>
+							<Form.Item>
+								<Button
+								type="primary"
+								htmlType="submit"
+								loading={loading}
+								className="bg-primary mt-4 px-4 -mr-[0.8rem] text-white max-sm:px-10 "
+								>
+								{loading ? 'Submitting...' : 'Submit'}
+								</Button>
+							</Form.Item>
+    					</Form>
 					</div>
 				</div>
 				<div>
